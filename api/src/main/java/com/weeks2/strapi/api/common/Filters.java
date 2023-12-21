ce5bcb176a2c9434cc11f368be73e5758dc8607d
Christian Ibarra <chris.cjarne@gmail.com>
2023-12-21 09:20:35 -0600

    Create Filters.java

---
+++ api/src/main/java/com/weeks2/strapi/api/common/Filters.java
@@ -0,0 +1,71 @@
+package com.weeks2.strapi.api.common;
+
+import java.util.Arrays;
+
+/**
+ * https://docs.strapi.io/dev-docs/api/rest/filters-locale-publication#filtering
+ * Queries can accept a filters parameter with the following syntax:
+ * GET /api/:pluralApiId?filters[field][operator]=value
+ */
+public enum Filters {
+    EQ("$eq", "Equal"),
+    EQI("$eqi", "Equal (case-insensitive)"),
+    NE("$ne", "Not equal"),
+    NEI("$nei", "Not equal (case-insensitive)"),
+    LT("$lt", "Less than"),
+    LTE("$lte", "Less than or equal to"),
+    GT("$gt", "Greater than"),
+    GTE("$gte", "Greater than or equal to"),
+    IN("$in", "Included in an array"),
+    NOT_IN("$notIn", "Not included in an array"),
+    CONTAINS("$contains", "Contains"),
+    NOT_CONTAINS("$notContains", "Does not contain"),
+    CONTAINS_I("$containsi", "Contains (case-insensitive)"),
+    NOT_CONTAINS_I("$notContainsi", "Does not contain (case-insensitive)"),
+    NULL("$null", "Is null"),
+    NOT_NULL("$notNull", "Is not null"),
+    BETWEEN("$between", "Is between"),
+    STARTS_WITH("$startsWith", "Starts with"),
+    STARTS_WITH_I("$startsWithi", "Starts with (case-insensitive)"),
+    ENDS_WITH("$endsWith", "Ends with"),
+    ENDS_WITH_I("$endsWithi", "Ends with (case-insensitive)"),
+    OR("$or", "Joins the filters in an 'or' expression"),
+    AND("$and", "Joins the filters in an 'and' expression"),
+    NOT("$not", "Joins the filters in a 'not' expression"),
+    KEY("$key", "Key");
+
+    private final String value;
+    private final String description;
+
+    Filters(String value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Filters getByValue(String value) {
+        return Arrays.stream(values())
+                .filter(operator -> operator.value.equalsIgnoreCase(value))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * GET /api/:pluralApiId?filters[field][operator]=value
+     */
+    public String addFilter(String uri, String field,Filters operator,String value) {
+        return new StringBuilder(uri).append(uri.contains("?") ? '&' : '?')
+                .append("filters[").append(field).append("][")
+                .append(operator.getValue())
+                .append("]=").append(value)
+                .toString();
+    }
+}
+
