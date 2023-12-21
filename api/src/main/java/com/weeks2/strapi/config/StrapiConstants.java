a62a7e5f2ee9a668c02e93be24c64cc1585eff3d
RecursoDidacticoDigital <nroac1700@alumno.ipn.mx>
2023-12-20 19:42:06 -0600

    added start_time & end_time

--- api/src/main/java/com/weeks2/strapi/config/StrapiConstants.java
+++ api/src/main/java/com/weeks2/strapi/config/StrapiConstants.java
@@ -2,4 +2,5 @@ package com.weeks2.strapi.config;
 
 public class StrapiConstants {
     public static final String pattern = "yyyy-MM-dd'T'HH:mm";
+    public static final String pattern_time = "HH:mm";
 }
--- api/src/main/java/com/weeks2/strapi/school/timeblock/Timeblock.java
+++ api/src/main/java/com/weeks2/strapi/school/timeblock/Timeblock.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.weeks2.strapi.config.StrapiConstants;
 import lombok.Data;
 
-import java.sql.Time;
+import java.util.Date;
 
 @Data
 public class Timeblock {
@@ -18,9 +18,15 @@ public class Timeblock {
     public static class Attributes{
         @JsonInclude(JsonInclude.Include.NON_NULL)
         Integer id;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = StrapiConstants.pattern)
+        private Date start;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = StrapiConstants.pattern)
+        private Date end;
         @JsonInclude(JsonInclude.Include.NON_NULL)
-        private String start;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = StrapiConstants.pattern_time)
+        private Date start_time;
         @JsonInclude(JsonInclude.Include.NON_NULL)
-        private String end;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = StrapiConstants.pattern_time)
+        private Date end_time;
     }
 }
