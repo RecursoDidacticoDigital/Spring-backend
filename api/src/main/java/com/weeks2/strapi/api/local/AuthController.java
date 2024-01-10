03465dbeb98f6dacc06d49ee7c36036f23b76986
RecursoDidacticoDigital <nroac1700@alumno.ipn.mx>
2024-01-09 18:54:37 -0600

    managed hashed password for members

--- api/src/main/java/com/weeks2/strapi/api/local/AuthController.java
+++ api/src/main/java/com/weeks2/strapi/api/local/AuthController.java
@@ -1,21 +1,25 @@
 package com.weeks2.strapi.api.local;
 
 import com.weeks2.strapi.api.common.AppEndPointsSchool;
+import com.weeks2.strapi.school.member.MemberService;
+import com.weeks2.strapi.school.member.auth.AuthMemberRequest;
+import com.weeks2.strapi.school.utility.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = AppEndPointsSchool.FLUTTER_APP_PATH)
 @Controller
 @RequestMapping(AppEndPointsSchool.AUTH_PATH)
 public class AuthController {
     @Autowired
-    private AuthService loginService;
+    private MemberService loginService;
 
     @PostMapping("/local")
-    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest authRequest) {
-        return loginService.auth(authRequest);
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthMemberRequest authMemberRequest){
+        // Hash the password
+        String passwordHash = SecurityUtils.toSHA256(authMemberRequest.getPassword());
+        return loginService.validateMemberAccount(authMemberRequest, passwordHash);
     }
 }
